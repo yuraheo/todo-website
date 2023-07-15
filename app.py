c32cfa81343ea19250065c9678ad8456053fa54e
yuraheo <122466495+yuraheo@users.noreply.github.com>
2023-07-15 06:50:00 +0000

    integrating streamlit
    integrating streamlit

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+# from flask import Flask, render_template
+# import streamlit as st
+# import pandas as pd
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#   return render_template('home.html')
 
 
-@app.route('/')
-def hello_world():
-  return "hello world"
+# @app.route('/main')
+# def main():
+#   st.title("My Tasks")
+#   menu = ["Create", "Read", "Update", "Delete", "About"]
+#   choice = st.sidebar.selectbox("Menu", menu)
+
+#   if choice == "Create":
+#     st.subheader("Add Item")
+
+#   elif choice == "Read":
+#     st.subheader("View Items")
+
+#   elif choice == "Update":
+#     st.subheader("Edit/Update Items")
+
+#   elif choice == "Delete":
+#     st.subheader("Delete Item")
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+# return render_template('main.html')
+
+# import db
+
+# db.init_app(app)
+
+# import auth
+
+# app.register_blueprint(auth.bp)
+
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', debug=True)
+#   main()
+
